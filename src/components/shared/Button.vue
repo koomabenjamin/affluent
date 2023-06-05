725a23f470b9c8716fbc7a08052270add4ec76b6
Benjamin Kooma <benjaminkooma@Benjamins-MacBook-Pro.local>
2023-06-05 08:20:15 +0300

    Initial commit and setup

---
+++ src/components/shared/Button.vue
@@ -0,0 +1,48 @@
+<template>
+  <button 
+    class="
+      bg-blue-800 
+      border-2
+      flex
+      item-center
+      space-x-1
+      px-2
+      py-1
+      border-white">
+    <div>
+      <Icon
+        icon="svg-spinners:180-ring-with-bg"
+        class="text-white"
+        width="20"
+        v-if="loader" />
+    </div>
+    <span
+      :class="`
+        text-lg
+        ${(dark) ? 'text-white':''}
+        ${(size === 'sm') ? 'px-1 py-0.5':''}
+        ${(size === 'md') ? 'px-4 py-2':''}
+        ${(size === 'lg') ? 'px-6 py-3':''}
+      `">
+      {{ label }}
+    </span>
+
+  </button>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+export interface ButtonProps {
+  type?: string, // fab-button, label, normal 
+  size?: string,
+  label: string | number,
+  dark?: boolean,
+  loader?: boolean,
+}
+
+defineProps<ButtonProps>();
+
+</script>
+
+<style></style>
---
+++ src/views/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import Button from "../components/shared/Button.vue";
+</script>
+
+<template>
+  <main>
+    <div class="
+        h-screen 
+        w-screen 
+        flex
+        flex-col
+        items-center 
+        justify-center 
+        font-sans">
+      All content will go here...
+
+      <Icon icon="mdi:youtube" width="300" class="h-20 w-20 text-rose-600" />
+
+      <Button label="Login" loader dark />
+
+    </div>
+  </main>
+</template>
