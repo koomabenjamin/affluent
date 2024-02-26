4fd4e4bad455656c0bb8b26ad045e5276017139a
Benjamin Kooma <kooma.benjamin@gmail.com>
2024-02-26 23:12:02 +0300

    Introduce types to the select components

--- src/components/shared/Select.vue
+++ src/components/shared/Select.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="w-full relative">
     <div class="relative w-full h-12 mt-1">
-      <input 
-        :id="props.id"
+      <input :id="props.id"
         class="block px-2.5 pb-2.5 border pt-4 h-full w-full text-sm text-gray-900 rounded-sm appearance-none focus:outline-none focus:ring-0 peer"
-        :name="props.name"
-        :class="[(props.error !== '') ? 'focus:border-red-600' : 'focus:border-blue-600']"
-        @input="$emit('update:optionName', $event.target.value); $emit('update:optionId', $event.target.value)" 
-        @focus="toggleSelectOptions()"
-        :type="props.type"
-        placeholder=" " />
+        :name="props.name" :class="[(props.error !== '') ? 'focus:border-red-600' : 'focus:border-blue-600']"
+        @input="$emit('update:optionName', $event.target.value); $emit('update:optionId', $event.target.value)"
+        @focus="toggleSelectOptions()" :type="props.type" placeholder=" " />
       <label
         :class="[(props.error !== '') ? 'peer-focus:text-red-600 peer-focus:dark:text-red-500' : 'peer-focus:text-blue-600 peer-focus:dark:text-blue-500']"
         class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
@@ -42,7 +38,7 @@
         " v-for="option in displayedOptions" :key="option.id" @click="selectOption(option)">
         <span class="text-sm text-blue-600">{{ option.name }}</span>
         <span class="text-xs text-gray-400"
-          v-if="(typeof option.description) !== 'undegined'">{{ option.description }}</span>
+          v-if="(typeof option.description) !== 'undefined'">{{ option.description }}</span>
       </div>
     </div>
     <div v-if="!optionsDropdown" class="
@@ -63,98 +59,115 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref, watch, onMounted } from "vue";
+<script lang="ts">
+import { ref, watch, onMounted, defineComponent } from "vue";
 import FloatingLabelInput from "./inputs/FloatingLabelInput.vue";
 
-const emit = defineEmits(["update:optionId", "update:optionName", "update:modelValue"]);
+interface SelectOptionStructure {
+  id?: string | number;
+  name?: string;
+}
 
-const selectedOptionId = ref("");
+export default defineComponent({
+  name: "Select",
+  props: {
+    id: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    hide: {
+      type: Boolean,
+      default: false,
+    },
+    optionId: {
+      type: [String, Number],
+    },
+    optionName: {
+      type: [String, Number],
+    },
+    label: {
+      type: String,
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    errors: {
+      type: Object,
+    },
+    options: {
+      type: [Array, Object],
+    },
+    required: {
+      type: Boolean,
+    },
+    disabled: {
+      type: Boolean,
+    },
+    value: {
+      type: [String, Number],
+    },
+    type: {
+      type: [String, Number, Symbol]
+    },
+    error: {
+      type: [String, Number, Array]
+    }
+  },
+  setup(props, { emit }) {
 
-const selectedOptionName = ref("");
+    defineEmits(["update:optionId", "update:optionName", "update:modelValue"]);
 
-const optionsDropdown = ref(false);
+    const selectedOptionId = ref("");
 
-const displayedOptions = ref([]);
+    const selectedOptionName = ref("");
 
-const toggleSelectOptions = () => optionsDropdown.value = !optionsDropdown.value;
+    const optionsDropdown = ref(false);
 
-const selectOption = (option:object) => {
-  optionsDropdown.value = false;
-  const inputField = document.getElementById(props.id);
-  // const hiddenInputField = document.getElementById(`${props.id}-hidden-field`);
-  inputField.dispatchEvent(new Event("input"));
-  inputField.value = option.name;
-  // hiddenInputField.value = option.id;
-  optionsDropdown.value = false;
-  emit("update:optionId", option.id);
-  emit("update:optionName", option.name);
-  emit("update:modelValue", option.name);
-};
+    const displayedOptions = ref([]);
 
-const props = defineProps({
-  id: {
-    type: String,
-  },
-  name: {
-    type: String,
-  },
-  hide: {
-    type: Boolean,
-    default: false,
-  },
-  optionId: {
-    type: [String, Number],
-  },
-  optionName: {
-    type: [String, Number],
-  },
-  label: {
-    type: String,
-  },
-  modelValue: {
-    type: [String, Number],
-    default: "",
-  },
-  errors: {
-    type: Object,
-  },
-  options: {
-    type: [Array, Object],
-  },
-  required: {
-    type: Boolean,
-  },
-  disabled: {
-    type: Boolean,
-  },
-  value: {
-    type: [String, Number],
-  },
-  errors: {
-    type: Object,
-  },
-  type: {
-    type: [String, Number, Symbol]
-  },
-  error: {
-    type: [String, Number, Array]
-  }
-});
+    const toggleSelectOptions = () => optionsDropdown.value = !optionsDropdown.value;
 
-watch(
-  () => props.optionName,
-  (optionName) => {
-    displayedOptions.value = props.options.filter((item) =>
-      item.name.toLowerCase().includes(optionName)
+    const selectOption = (option: SelectOptionStructure) => {
+      optionsDropdown.value = false;
+      const inputField: HTMLElement = document.getElementById(props?.id ?? '');
+      // const hiddenInputField = document.getElementById(`${props.id}-hidden-field`);
+      inputField.dispatchEvent(new Event("input"));
+      inputField.value = option.name;
+      // hiddenInputField.value = option.id;
+      optionsDropdown.value = false;
+      emit("update:optionId", option.id);
+      emit("update:optionName", option.name);
+      emit("update:modelValue", option.name);
+    };
+
+    watch(
+      () => props.optionName,
+      (optionName) => {
+        displayedOptions.value = props.options.filter((item) =>
+          item.name.toLowerCase().includes(optionName)
+        );
+        if (optionsDropdown.value === false) optionsDropdown.value = true;
+      }
     );
-    if (optionsDropdown.value === false) optionsDropdown.value = true;
-  }
-);
 
-onMounted(() => {
-  displayedOptions.value = props.options;
+    onMounted(() => {
+      displayedOptions.value = props.options;
+    });
+
+    return {
+      selectOption,
+      toggleSelectOptions,
+      selectedOptionId,
+      selectedOptionName,
+      displayedOptions,
+      optionsDropdown
+    }
+  },
 });
+
 </script>
 
 <style scoped>
