ffad9bc6421d0109df59d3db62654cd4ff0a8b36
Benjamin Kooma <bkooma@projectcode.ug>
2024-08-13 16:32:46 +0300

    Finish loan-book structure

--- src/components/shared/DataTable.vue
+++ src/components/shared/DataTable.vue
@@ -1,52 +1,49 @@
 <template>
-  <div class="w-full overflow-auto">
+  <table class="lg:w-full h-[1000px] mt-4 overflow-auto">
 
-    <table class="lg:w-full w-[800px] mt-4 overflow-auto">
-
-      <thead>
-        <tr class="">
-          <th></th>
-          <th v-for="column in columns" :key="column" class="capitalize text-left text-xs h-10 whitespace-nowrap">
-            {{ removeSpecialCharacter(column) }}
-          </th>
-          <th v-if="actions" class="capitalize text-left text-xs h-10">
-            <!--SLOT FOR CUSTOM ACTIONS FOR ANY COMPONENT THAT USES THE DATATABLE-->
-            <!-- <slot name="actions"></slot> -->
-            Actions
-          </th>
-        </tr>
-      </thead>
-
-      <tr v-for="(value, index) in rows" :key="index" class="bg-slate-50 border-b">
-
-        <td class="text-xs">{{ getRowId(index.toString()) }}.</td>
-
-        <td v-for="column in columns" :key="column" class="text-xs pl-2 pr-2 py-4">
-          <div v-if="!customSlots?.includes(column)">{{ value[column] }}</div>
-          <slot v-else :name="column" :data="value[column]"></slot>
-        </td>
-
-        <td v-if="actions">
-          <slot name="actions"></slot>
-        </td>
-
+    <thead>
+      <tr class="">
+        <th></th>
+        <th v-for="column in columns" :key="column" class="capitalize text-left text-xs h-10 whitespace-nowrap">
+          {{ removeSpecialCharacter(column) }}
+        </th>
+        <th v-if="actions" class="capitalize text-left text-xs h-10">
+          <!--SLOT FOR CUSTOM ACTIONS FOR ANY COMPONENT THAT USES THE DATATABLE-->
+          <!-- <slot name="actions"></slot> -->
+          Actions
+        </th>
       </tr>
-      <tfoot v-if="(rows?.length ?? 0) > 10">
-        <tr>
-          <th></th>
-          <th v-for="column in columns" :key="column" class="capitalize text-sm text-left h-10">
-            {{ removeSpecialCharacter(column) }}
-          </th>
-          <th v-if="actions" class="capitalize text-left text-xs h-10">
-            <!--SLOT FOR CUSTOM ACTIONS FOR ANY COMPONENT THAT USES THE DATATABLE-->
-            <!-- <slot name="actions"></slot> -->
-            Actions
-          </th>
-        </tr>
-      </tfoot>
+    </thead>
 
-    </table>
-  </div>
+    <tr v-for="(value, index) in rows" :key="index" class="bg-slate-50 border-b">
+
+      <td class="text-xs">{{ getRowId(index.toString()) }}.</td>
+
+      <td v-for="column in columns" :key="column" class="text-xs pl-2 pr-2 py-4">
+        <div v-if="!customSlots?.includes(column)">{{ value[column] }}</div>
+        <slot v-else :name="column" :data="value[column]"></slot>
+      </td>
+
+      <td v-if="actions">
+        <slot name="actions"></slot>
+      </td>
+
+    </tr>
+    <tfoot v-if="(rows?.length ?? 0) > 10">
+      <tr>
+        <th></th>
+        <th v-for="column in columns" :key="column" class="capitalize text-xs text-left h-10">
+          {{ removeSpecialCharacter(column) }}
+        </th>
+        <th v-if="actions" class="capitalize text-left text-xs h-10">
+          <!--SLOT FOR CUSTOM ACTIONS FOR ANY COMPONENT THAT USES THE DATATABLE-->
+          <!-- <slot name="actions"></slot> -->
+          Actions
+        </th>
+      </tr>
+    </tfoot>
+
+  </table>
   <NoData :label="dataGroupName" v-if="rows?.length === 0" />
 </template>
 
