2b1bd8525680f83b867e6755e9ff7696c4573f44
Benjamin Kooma <kooma.benjamin@gmail.com>
2025-07-03 13:02:29 +0300

    Introduce newfeed component on landing page...

--- src/views/main/NewsFeedPage.vue
+++ src/views/main/NewsFeedPage.vue
@@ -87,7 +87,7 @@ const appName = import.meta.env.VITE_CLIENT_GROUP_NAME;
         Total 12 (delta 9), reused 0 (delta 0), pack-reused 0
         remote: Resolving deltas: 100% (9/9), completed with 9 local objects.
         To https://github.com/koomabenjamin/affluent.git
-        53e5083..5dbefd1 work-flow-v2.0 -> work-flow-v2.0Counting objects: 100% (23/23), done.
+        53e5083..5dbefd1 work-flow-v2.0 -> work-flow-v2.0 Counting objects: 100% (23/23), done.
         Delta compression using up to 8 threads
         Compressing objects: 100% (12/12), done.
         Writing objects: 100% (12/12), 1.59 KiB | 1.59 MiB/s, done.
